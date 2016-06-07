0ef433340d8e96230ddd21d3d2ebfcdcb7657e02
Moriadry <dp11@software.nju.edu.cn>
2016-06-08 01:58:56 +0800

    add comments

---
+++ src/SingleNumber/Solution.java
@@ -0,0 +1,16 @@
+package SingleNumber;
+
+/**
+ * Created by dingpeng on 16/6/7.
+ */
+public class Solution {
+
+    //perfect solution
+    public int  singleNumber(int[] nums) {
+        int res=nums[0];
+        for(int i=1;i<nums.length;i++)
+            res=(int)((res|~nums[i]) & (~res|nums[i]));
+        //(a or !b) and (!a or b) 1 for same; 0 for  different
+        return res;
+    }
+}
