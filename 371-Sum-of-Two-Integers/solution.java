f2dc6929dee3385f66471439bb219171e8d6bb69
myPrecious <Moriadry@users.noreply.github.com>
2016-08-10 03:15:04 +0800

    [371. Sum of Two Integers][Accepted]committed by LeetCode Extension

---
+++ 371-Sum-of-Two-Integers/solution.java
@@ -0,0 +1,12 @@
+public class Solution {
+    public int getSum(int a, int b) {
+        int sum,carry;
+		do{
+			sum = a^b;
+			carry = (a&b)<<1;
+			a = sum;
+			b = carry;
+		}while(carry!=0);
+		return sum;
+    }
+}
