af9fdf7bbf601538f939e7254de9309c0a44e638
myPrecious <Moriadry@users.noreply.github.com>
2016-08-11 02:08:57 +0800

    [292. Nim Game][Compile Error]committed by LeetCode Extension

--- 292-Nim-Game/solution.java
+++ 292-Nim-Game/solution.java
@@ -1,11 +1,4 @@
-public class Solution {
-    public boolean canWinNim(int n) {
-        if(n==1 || n==2 || n==3) {
-            return true;
-        } else if(n ==4 ) {
-            return false;
-        } else {
-            return canWinNim(n-1);
-        }
-    }
+public boolean canWinNim(int n) { 
+    return !(n%4==0) ; 
+    
 }
