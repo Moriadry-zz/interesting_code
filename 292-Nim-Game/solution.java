9c59ac41a7ff38a73aa3d11a3c39378be89418ce
myPrecious <Moriadry@users.noreply.github.com>
2016-08-11 02:01:07 +0800

    [292. Nim Game][Runtime Error]committed by LeetCode Extension

--- 292-Nim-Game/solution.java
+++ 292-Nim-Game/solution.java
@@ -1,11 +1,11 @@
 public class Solution {
     public boolean canWinNim(int n) {
-        if(1<= n <=3) {
+        if(n==1 || n==2 || n==3) {
             return true;
         } else if(n ==4 ) {
             return false;
         } else {
-            return canWinNim(n--);
+            return !canWinNim(n--);
         }
     }
 }
