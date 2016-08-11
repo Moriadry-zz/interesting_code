b4657306fae4f3d1101f8046f74369c8f2410e22
myPrecious <Moriadry@users.noreply.github.com>
2016-08-12 01:48:34 +0800

    [378. Kth Smallest Element in a Sorted Matrix][Accepted]committed by LeetCode Extension

--- 378-Kth-Smallest-Element-in-a-Sorted-Matrix/solution.java
+++ 378-Kth-Smallest-Element-in-a-Sorted-Matrix/solution.java
@@ -1,21 +1,13 @@
 public class Solution {
      public int kthSmallest(int[][] matrix, int k) {
-        int n = matrix.length;
-        int[] rows = new int[n];
-        int result = matrix[0][0];
-        while (k-- > 0) {
-            int smallest = Integer.MAX_VALUE;
-            int colOfSmallest = 0;
-            for (int col = 0; col < n; col++) {
-                if (rows[col] == n) { continue; }
-                if (matrix[rows[col]][col] <= smallest) {
-                    smallest = matrix[rows[col]][col];
-                    colOfSmallest = col;
-                    result = smallest;
-                }
+        if(k == 1) return matrix[0][0];
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int row = 0; row < matrix.length; row++){
+            for(int col = 0; col < matrix[row].length; col++){
+                list.add(matrix[row][col]);
             }
-            rows[colOfSmallest]++;
         }
-        return result;
+        Collections.sort(list);
+        return list.get(k-1);
     }
 }
